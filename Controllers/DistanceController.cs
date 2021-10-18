97b9541f498ce94a3024b14eace1902c1c5f80ad
arevars <arevars@gmail.com>
2021-10-18 21:51:58 +0400

    first commit

---
+++ Controllers/DistanceController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistanceService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DistanceController : ControllerBase
+    {
+        private readonly ILogger<DistanceController> _logger;
+        private readonly IMapper _mapper;
+
+        public DistanceController(ILogger<DistanceController> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        //[HttpGet]
+        //public IActionResult GetDistance()
+        //{
+        //    return 
+        //}
+    }
+}
